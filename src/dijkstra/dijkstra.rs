2cc4c604be8013fe09dbf7f0321c2ec65b0b7826
VolkerFelix <volker.leukhardt@gmail.com>
2024-01-22 22:00:03 +0000

    Debug dijkstra

--- src/dijkstra/dijkstra.rs
+++ src/dijkstra/dijkstra.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::LowerExp};
+use std::collections::HashMap;
 use crate::graph::graph::{Graph, Node};
 
 pub fn dijkstra<'a, 'b>(f_graph: &'a Graph) -> Option<Graph<'b>> {
@@ -13,31 +13,42 @@ pub fn dijkstra<'a, 'b>(f_graph: &'a Graph) -> Option<Graph<'b>> {
         parents.insert(&start, edge.m_connects);
     }
     let temp_costs = costs.clone();
-    let lowest_weight = temp_costs.iter().min().unwrap();
-    let neighbors_of_lowest_weight = &lowest_weight.0.m_connections;
 
-    for neighbor in neighbors_of_lowest_weight {
-        // Calculate new cost from start
-        let new_cost = lowest_weight.1 + neighbor.m_weight.unwrap();
-        // Compare the costs; If lower then update them
-        if costs.get(neighbor.m_connects).unwrap() > &new_cost {
-            // Found a better way!
-            costs.insert(&neighbor.m_connects, new_cost);
-            parents.insert(&neighbor.m_connects, lowest_weight.0);
+    let mut lowest_weight_node = find_lowest_weight_node(&costs, &already_processed);
+
+    while lowest_weight_node.is_some() {
+        let current_node = lowest_weight_node.unwrap();
+        // Get all neighbours
+        for neighbour in &current_node.m_connections {
+            // Get cost to get to current node
+            let current_node_cost = costs.get(current_node).unwrap();
+            // Calculate new total cost to get to this neighbour
+            let new_cost = current_node_cost + neighbour.m_weight.unwrap();
+            // Compare with existing costs
+            if costs.get(neighbour.m_connects).unwrap() > &new_cost {
+                // Found a cheaper way! --> Update costs and parent
+                costs.insert(neighbour.m_connects, new_cost);
+                parents.insert(neighbour.m_connects, current_node);
+            }
         }
+        already_processed.push(current_node);
+        lowest_weight_node = find_lowest_weight_node(&costs, &already_processed);
     }
-    already_processed.push(lowest_weight.0);
-
+    for (child, parent) in parents.iter() {
+        println!("Final result: ");
+        println!("Child: {}, Parent: {}", child.m_name, parent.m_name);
+    }
     None
 }
 
-fn find_lowest_weight_node(f_node_map: HashMap<&Node, usize>, f_aready_processed: &Vec<&Node>) -> Option<&Node> {
-    let lowest_weight_node = f_node_map.iter().min();
-    match lowest_weight_node {
-        None => return None,
-        Some(node) => {
-            // Already processed?
-            // TODO: Continue here
+fn find_lowest_weight_node<'a>(f_node_map: &HashMap<&'a Node, usize>, f_already_processed: &Vec<&Node>) -> Option<&'a Node<'a>> {
+    let mut lowest_cost = usize::MAX;
+    let mut lowest_cost_node = None;
+    for (node, weight) in f_node_map {
+        if weight < &lowest_cost && !f_already_processed.contains(&node) {
+            lowest_cost = *weight;
+            lowest_cost_node = Some(*node);
         }
     }
+    lowest_cost_node
 }
